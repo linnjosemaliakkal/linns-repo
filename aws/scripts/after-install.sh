5543cefc5448f457aabad4486bef0192c7147255
linnjosemaliakkal <linnjosemaliakkal@gmail.com>
2025-10-02 19:02:33 -0400

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -4,16 +4,12 @@ set -e
 WAR_FILE=spring-boot-hello-world-example-1.0-SNAPSHOT.war
 TOMCAT_HOME=/usr/share/tomcat/webapps
 
+echo "=== AfterInstall: Deploying WAR ==="
 
-echo "=== AfterInstall: Copying WAR to Tomcat webapps ==="
-
-# Optionally copy WAR from S3 if needed
-# aws s3 cp s3://codedeploystack-webappdeploymentbucket-seqdgxefq2qg/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
-
-# Ensure tomcat directory exists
+# Ensure Tomcat webapps folder exists
 mkdir -p $TOMCAT_HOME
 
-# Copy WAR from /tmp (where CodeDeploy placed it) to Tomcat
+# Copy WAR file from CodeDeploy staging (/tmp) to Tomcat
 cp /tmp/$WAR_FILE $TOMCAT_HOME/
 
 echo "WAR copied to $TOMCAT_HOME"
