c9f1b5bc250c6c0c74d63c14781063411b090584
linnjosemaliakkal <linnjosemaliakkal@gmail.com>
2025-10-02 18:04:42 -0400

    Add all untracked files and update tracked files

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+<<<<<<< HEAD
 set -xe
 
 # Start Tomcat, the application server.
 service tomcat start
+=======
+set -e
+
+echo "=== Starting Tomcat service ==="
+systemctl start tomcat || systemctl restart tomcat
+>>>>>>> 9981c63 (Add all untracked files and update tracked files)
